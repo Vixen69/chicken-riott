03b54e7e66b8d2e24323d7499f496ac6845e6139
Vixen69 <revelin@live.be>
2015-03-23 18:44:17 +0100

    chicken

---
+++ siso.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class siso : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (0, 5, 0);
+	
+	}
+}
